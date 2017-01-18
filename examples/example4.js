22a2ced8d1a3551499f0c0cc61692bbdf5e78973
DyslexicDcuk <dsehic@gmail.com>
2017-01-18 12:25:17 +0100

    expand example4

--- examples/example4.js
+++ examples/example4.js
@@ -1,8 +1,15 @@
 const $input = document.querySelector('#myInput')
 
 Rx.Observable.fromEvent($input, 'keyup')
-  .map((v) => v.target.value)
-  .filter((v) => v.length > 2)
-  .debounceTime(500)
-  .distinctUntilChanged()
+  // .map((v) => v.target.value)
+  // .filter((v) => v.length > 2)
+  // .debounceTime(500)
+  // .distinctUntilChanged()
   .subscribe((v) => console.log(v))
+
+// .switchMap(searchTerm => apiService.getData(searchTerm))
+// .subscribe(
+//   (res) => console.log('Success: ' + res),
+//   (err) => console.error('Error: ' + err),
+//   () => console.log('Done!'),
+// )
